af014dcc7b4cc8e2b130a631513c9bd026c86be5
Rongmario <rongmario@proton.me>
2025-03-11 03:25:41 +0000

    Differentiate JAVA_HOME env and java.home property

--- src/main/java/com/cleanroommc/javautils/locators/JavaHomeJavaLocator.java
+++ src/main/java/com/cleanroommc/javautils/locators/JavaHomeJavaLocator.java
@@ -3,24 +3,24 @@ package com.cleanroommc.javautils.locators;
 import com.cleanroommc.javautils.JavaUtils;
 import com.cleanroommc.javautils.api.JavaInstall;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JavaHomeJavaLocator extends AbstractJavaLocator {
 
     @Override
     protected List<JavaInstall> initialize() {
-        String javaHome = null;
+        List<JavaInstall> javaInstalls = new ArrayList<>();
+        String javaHomeEnv = null;
         try {
-            javaHome = env("JAVA_HOME");
+            javaHomeEnv = env("JAVA_HOME");
         } catch (SecurityException ignore) { }
-        if (javaHome == null) {
-            javaHome = property("java.home");
-            if (javaHome == null) {
-                return Collections.emptyList();
-            }
+        if (javaHomeEnv != null) {
+            javaInstalls.add(JavaUtils.parseInstall(javaHomeEnv));
         }
-        return Collections.singletonList(JavaUtils.parseInstall(javaHome));
+        String current = property("java.home");
+        javaInstalls.add(JavaUtils.parseInstall(current));
+        return javaInstalls;
     }
 
 }
