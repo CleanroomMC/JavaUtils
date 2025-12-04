d12736ad70a15cde3074e749fb1bedb0f954fd23
Rongmario <rongmario@proton.me>
2025-12-04 01:56:58 +0000

    Add PrismLauncher paths to detect from

--- src/main/java/com/cleanroommc/javautils/locators/MinecraftJavaLocator.java
+++ src/main/java/com/cleanroommc/javautils/locators/MinecraftJavaLocator.java
@@ -5,7 +5,6 @@ import com.cleanroommc.platformutils.Platform;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class MinecraftJavaLocator extends AbstractJavaLocator {
@@ -13,41 +12,57 @@ public class MinecraftJavaLocator extends AbstractJavaLocator {
     @Override
     protected List<JavaInstall> initialize() {
         List<JavaInstall> installs = new ArrayList<>();
-        Platform current = Platform.current();
-        switch (current.operatingSystem()) {
+        switch (Platform.current().operatingSystem()) {
             case WINDOWS:
-                List<File> programFilesLocations = new ArrayList<>();
-                String programFiles = env("ProgramFiles");
-                if (programFiles != null) {
-                    programFilesLocations.add(new File(programFiles));
-                }
-                programFiles = env("ProgramFiles(x86)");
-                if (programFiles != null) {
-                    programFilesLocations.add(new File(programFiles));
-                }
-                programFiles = env("ProgramFiles(Arm)");
-                if (programFiles != null) {
-                    programFilesLocations.add(new File(programFiles));
-                }
-                for (File directory : programFilesLocations) {
-                    if (directory.exists()) {
-                        deepScanForInstalls(new File(directory, "Minecraft Launcher/runtime"), installs);
-                    }
-                }
-
-                File appData = new File(env("LOCALAPPDATA"));
-                deepScanForInstalls(new File(appData, "Packages/Microsoft.4297127D64EC6_8wekyb3d8bbwe/LocalCache/Local/runtime"), installs);
-                deepScanForInstalls(new File(appData, "Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalCache/Local/runtime"), installs);
-                deepScanForInstalls(new File(appData, "Packages/Microsoft.MinecraftJavaEdition_8wekyb3d8bbwe/LocalCache/Local/runtime"), installs);
-                break;
-            case LINUX:
-                deepScanForInstalls(new File(userHome(".minecraft/runtime")), installs);
+                this.windows(installs);
                 break;
             case MAC_OS:
-                deepScanForInstalls(new File(userHome("Library/Application Support/minecraft/runtime")), installs);
+                this.macOs(installs);
                 break;
+            default:
+                this.linux(installs);
         }
-        return Collections.emptyList();
+        return installs;
+    }
+
+    private void windows(List<JavaInstall> installs) {
+        List<File> programFilesLocations = new ArrayList<>();
+        String programFiles = env("ProgramFiles");
+        if (programFiles != null) {
+            programFilesLocations.add(new File(programFiles));
+        }
+        programFiles = env("ProgramFiles(x86)");
+        if (programFiles != null) {
+            programFilesLocations.add(new File(programFiles));
+        }
+        programFiles = env("ProgramFiles(Arm)");
+        if (programFiles != null) {
+            programFilesLocations.add(new File(programFiles));
+        }
+        for (File directory : programFilesLocations) {
+            if (directory.exists()) {
+                deepScanForInstalls(new File(directory, "Minecraft Launcher/runtime"), installs);
+            }
+        }
+
+        deepScanForInstalls(new File(userHome("scoop/persist/prismlauncher/java")), installs);
+        File appData = new File(env("LOCALAPPDATA"));
+        deepScanForInstalls(new File(appData, "Packages/Microsoft.4297127D64EC6_8wekyb3d8bbwe/LocalCache/Local/runtime"), installs);
+        deepScanForInstalls(new File(appData, "Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalCache/Local/runtime"), installs);
+        deepScanForInstalls(new File(appData, "Packages/Microsoft.MinecraftJavaEdition_8wekyb3d8bbwe/LocalCache/Local/runtime"), installs);
+        File roaming = new File(env("APPDATA"));
+        deepScanForInstalls(new File(roaming, "PrismLauncher/java"), installs);
+    }
+
+    private void macOs(List<JavaInstall> installs) {
+        deepScanForInstalls(new File(userHome("Library/Application Support/minecraft/runtime")), installs);
+        deepScanForInstalls(new File(userHome("Library/Application Support/PrismLauncher/java")), installs);
+    }
+
+    private void linux(List<JavaInstall> installs) {
+        deepScanForInstalls(new File(userHome(".minecraft/runtime")), installs);
+        deepScanForInstalls(new File(userHome(".local/share/PrismLauncher/java")), installs);
+        deepScanForInstalls(new File(userHome(".var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher")), installs);
     }
 
 }
